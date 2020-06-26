d5f297daac4cbb4082bb13f58abbb87ddc3f0adf
Jacob Makarsky <jmm7293@psu.edu>
2020-06-25 20:18:50 -0400

    editing package.json again cuz the homepage url needs to be my website link, not github pages link since i have custom domain...

---
+++ build/precache-manifest.7e30736e656ece5ef415e37af3cf7251.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1537397b858ca31c278a1aeac15d2054",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0785100c1dd907d5b964",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "86fa7c73e0dbe3218a3f",
+    "url": "/static/css/main.1ebc17af.chunk.css"
+  },
+  {
+    "revision": "0785100c1dd907d5b964",
+    "url": "/static/js/2.0e0bf797.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0e0bf797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86fa7c73e0dbe3218a3f",
+    "url": "/static/js/main.fb561759.chunk.js"
+  },
+  {
+    "revision": "67bddcda3ab8ab30a714",
+    "url": "/static/js/runtime-main.2791205e.js"
+  },
+  {
+    "revision": "02549161d8f6ca658996ab4bc3a31744",
+    "url": "/static/media/forbes.02549161.JPG"
+  },
+  {
+    "revision": "540c0bfe4e64012acbb9ee42e3b1a82c",
+    "url": "/static/media/hackpsu.540c0bfe.jpeg"
+  },
+  {
+    "revision": "dec78884646b16d7a5d3c023c00e6f65",
+    "url": "/static/media/headerBackground.dec78884.jpg"
+  },
+  {
+    "revision": "b181550c3ca6c6c3dcdb7acae6a5d293",
+    "url": "/static/media/innoblue.b181550c.JPG"
+  },
+  {
+    "revision": "808354bf219872b0eed1bf8bd0488b2c",
+    "url": "/static/media/jacob.808354bf.jpg"
+  },
+  {
+    "revision": "14d007cc684c409dad6bfac1191767fa",
+    "url": "/static/media/loopring.14d007cc.JPG"
+  },
+  {
+    "revision": "9af1358c5f5ac049a5c2944a7bdb7efd",
+    "url": "/static/media/me.9af1358c.JPG"
+  },
+  {
+    "revision": "f96128fb89bc1f122a38cbcca836d7bc",
+    "url": "/static/media/nittanyai.f96128fb.JPG"
+  },
+  {
+    "revision": "97f188b026f6137c8f9512c3c7e78ffa",
+    "url": "/static/media/slideshowBackground.97f188b0.jpg"
+  },
+  {
+    "revision": "71388b170df8454a0217a5e2ee5e8ea3",
+    "url": "/static/media/website.71388b17.jpg"
+  },
+  {
+    "revision": "ee57dab1e428ef9389c84f608adf072c",
+    "url": "/static/media/workshop1.ee57dab1.JPG"
+  }
+]);
