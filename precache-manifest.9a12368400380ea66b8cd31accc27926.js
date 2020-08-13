099797e77bb509ad87984a07b779302c85d17e3c
Jacob Makarsky <jmm7293@psu.edu>
2020-08-13 12:08:07 -0400

    Updates

--- precache-manifest.9a12368400380ea66b8cd31accc27926.js
+++ precache-manifest.9a12368400380ea66b8cd31accc27926.js
@@ -1,39 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e7f4f903d58f91d992c2f7ebf90d8fc6",
+    "revision": "a124013c68d457a5ca65da9c23207b24",
     "url": "/index.html"
   },
   {
-    "revision": "2588875590967956ca5a",
+    "revision": "4fdcc7944d7ac5c55529",
     "url": "/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "566cb696c04f7a8e19e8",
+    "revision": "2e76a5f7337d80e1326b",
     "url": "/static/css/main.046e09ba.chunk.css"
   },
   {
-    "revision": "2588875590967956ca5a",
-    "url": "/static/js/2.a06a9a97.chunk.js"
+    "revision": "4fdcc7944d7ac5c55529",
+    "url": "/static/js/2.fa1e7f19.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/static/js/2.a06a9a97.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.fa1e7f19.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "566cb696c04f7a8e19e8",
-    "url": "/static/js/main.6fea32a9.chunk.js"
+    "revision": "2e76a5f7337d80e1326b",
+    "url": "/static/js/main.92c617e1.chunk.js"
   },
   {
     "revision": "67bddcda3ab8ab30a714",
     "url": "/static/js/runtime-main.2791205e.js"
-  },
-  {
-    "revision": "02549161d8f6ca658996ab4bc3a31744",
-    "url": "/static/media/forbes.02549161.JPG"
-  },
-  {
-    "revision": "540c0bfe4e64012acbb9ee42e3b1a82c",
-    "url": "/static/media/hackpsu.540c0bfe.jpeg"
   },
   {
     "revision": "dec78884646b16d7a5d3c023c00e6f65",
@@ -42,14 +34,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "b181550c3ca6c6c3dcdb7acae6a5d293",
     "url": "/static/media/innoblue.b181550c.JPG"
-  },
-  {
-    "revision": "808354bf219872b0eed1bf8bd0488b2c",
-    "url": "/static/media/jacob.808354bf.jpg"
-  },
-  {
-    "revision": "14d007cc684c409dad6bfac1191767fa",
-    "url": "/static/media/loopring.14d007cc.JPG"
   },
   {
     "revision": "9af1358c5f5ac049a5c2944a7bdb7efd",
@@ -62,10 +46,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "f96128fb89bc1f122a38cbcca836d7bc",
     "url": "/static/media/nittanyai.f96128fb.JPG"
-  },
-  {
-    "revision": "97f188b026f6137c8f9512c3c7e78ffa",
-    "url": "/static/media/slideshowBackground.97f188b0.jpg"
   },
   {
     "revision": "71388b170df8454a0217a5e2ee5e8ea3",
